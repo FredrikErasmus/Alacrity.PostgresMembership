fa37cf6fe90921de1f0d4eeb82ccfd8e1df285ef
fredrik.erasmus <fredrik.erasmus@alacrity.co.za>
2015-04-01 12:24:32 +0200

    first commit

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Alacrity.PostgresMembership.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Alacrity.PostgresMembership.Startup))]
+namespace Alacrity.PostgresMembership
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
